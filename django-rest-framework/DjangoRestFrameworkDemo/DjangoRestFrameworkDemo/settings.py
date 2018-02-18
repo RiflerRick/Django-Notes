d35adab52e593115422b151f744fa6fc35c6cd95
RiflerRick <rajdeep.mukherjee295@gmail.com>
2018-02-18 16:42:37 +0530

    settings added for mysql database

--- django-rest-framework/DjangoRestFrameworkDemo/DjangoRestFrameworkDemo/settings.py
+++ django-rest-framework/DjangoRestFrameworkDemo/DjangoRestFrameworkDemo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DjangoRestFrameworkDemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'API_Demo',
+        'USER': 'root',
+        'PASSWORD': os.environ['mysql_root_password'],
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
